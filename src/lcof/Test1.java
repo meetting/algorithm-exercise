9b979292d5898ffa06972738f095b979015f04c9
yulaoda <841123576@qq.com>
2021-05-28 01:49:22 +0800

    Feat：算法练习项目创建。

---
+++ src/lcof/Test1.java
@@ -0,0 +1,13 @@
+package lcof;
+
+/**
+ * @Author: 余新洁
+ * @Date: 2021/5/28
+ * @Description:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+
+}
